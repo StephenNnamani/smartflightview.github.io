9a4b42d45d7bc42cacd73c64c263f7c52f4e6e09
StephenNnamani <sir.stephennnamani@gmail.com>
2022-03-05 14:39:16 +0100

    update

--- index.js
+++ index.js
@@ -1,28 +1,80 @@
 const express                           = require('express');
 const app                               = express();
-const mongodb                           = require('mongodb');
+const mysql                             = require('mysql');
 const bodyparser                        = require('body-parser');
 const fs                                = require('fs');
 const http                              = require('http');
 const express_session                   = require('express-session');
-const port                              = "3000"
+const port                              = process.env.port
+const dotenv                            = require('dotenv').config()
+var Amadeus                             = require('amadeus');
+
+// ------------------------ AMADEUS CALLS ---------------------------
+// var amadeus = new Amadeus({
+//   clientId: process.env['AMADEUS_CLIENT_ID'],
+//   clientSecret: process.env['AMADEUS_CLIENT_SECRET']
+// });
+
+var amadeus = new Amadeus({
+  clientId:       process.env.AMADEUS_CLIENT_ID,
+  clientSecret:   process.env.AMADEUS_CLIENT_SECRET
+})
+
+// fetching data from HTML files
+// let townFrom = document.getElementById('townFrom').value
+
 
 // ----------------------- External CSS and Images server ------------------
 app.use("/Public", express.static('./Public/'));
 
 // -------------------------- Importing routes ----------------------
-var hotelRouter = require('../smartflightview.github.io/routes/hotels')
-var flightRouter = require('../smartflightview.github.io/routes/flights')
-var visaRouter  = require('../smartflightview.github.io/routes/visa')
+var hotelRouter   = require('../smartflightview.github.io/routes/hotels')
+var flightRouter  = require('../smartflightview.github.io/routes/flights')
+var visaRouter    = require('../smartflightview.github.io/routes/visa')
 
 // Getting all the necessary Routes
 // -------------- Index -------------------------
-app.get('/', (req, res)=>{
+// var townFrom            = "ABJ",  countryTo           = "SYN",  departureDate       = "2022-02-29",
+//   returnDate          = "2023-04-22",
+//   adults              = 3,
+//   minor               = 5,
+//   flightClass         = "Economy"
+// fetch('https://test.api.amadeus.com/v3').then((data)=>{
+//         console.log(data)
+//     })
+    amadeus.shopping.flightOffersSearch.get({ 
+      originLocationCode: 'BOS', 
+      destinationLocationCode: 'LON', 
+      departureDate: '2020-08-01', 
+      adults: '2' 
+    }).then(function (data) { 
+      console.log(JSON.stringify(data)); 
+    }).catch(function (responseError) { 
+      console.log(JSON.stringify(responseError)); 
+    }); 
+
+  app.get('/', (req, res)=>{
     res.render('index.ejs')
-    fetch('https://test.api.amadeus.com/v3').then((data)=>{
-        console.log(data)
-    })
-})
+// app.get('/', (req, res)=>{
+//     res.render('index.ejs')
+//     fetch('https://test.api.amadeus.com/v3').then((data)=>{
+//         console.log(data)
+//     })
+//     amadeus.shopping.flightOffersSearch.get({
+//       originLocationCode:             `${townFrom}`,
+//       destinationLocationCode:        `${countryTo}`,
+//       departureDate:                  `${departureDate}`,
+//       returnDate:                     `${returnDate}`,
+//       adults:                         `${adults}`,
+//       minor:                          `${minor}`,
+//       flightClass:                    `${flightClass}`
+//   }).then(function(response){
+//     console.log(response.data);
+//   }).catch(function(responseError){
+//     console.log(responseError.code);
+//   });
+  
+ })
 // ----------------- Other External Routes -----------------------
 app.get('/hotel', hotelRouter)
 app.get('/flight%20search', flightRouter)
--- routes/hotels.js
+++ routes/hotels.js
@@ -4,10 +4,11 @@ const router = express()
 router.get('/hotel', (req, res)=>{
     res.render('hotel.ejs')
 })
-router.get('/hote/hotel%20search', (req, res)=>{
+router.get('/hote/hotelsearch', (req, res)=>{
     let hotels = fetch('https://test.api.amadeus.com/v3').then((data)=>{
         console.log(data)
     }).catch('there is an error')
+    console.log(hotels)
 })
 
 module.exports = router
